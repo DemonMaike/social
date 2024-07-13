d71e138e6b8c59e816798a0311541a4a2f598e86
Dmitry Malyshev <dd89268147556@gmail.com>
2024-07-14 00:50:51 +0400

    Added auth through base classes, added start page.

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -15,6 +15,10 @@ from configs import settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_REDIRECT_URL = 'account:dashboard'
+LOGIN_URL = 'account:login'
+LOGOUT_URL = 'account:logout'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
