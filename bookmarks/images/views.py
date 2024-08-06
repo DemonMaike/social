cf210d45a81a09b4cdfd3cef21b5ea69580d7313
Dmitry Malyshev <dd89268147556@gmail.com>
2024-08-06 17:51:56 +0400

    Update. Added form for work with bookmarks and Images

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from requests import request
+from .forms import ImageCreateForms
 
-# Create your views here.
+@login_required
+def image_create(request):
+    if request.method == "POST":
+        form = ImageCreateForms(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+            new_image.user = request.user
+            new_image.save()
+            messages.success(request, "Image added successfully")
+            
+            return redirect(new_image.get_absolute_url())
+    else:
+        form = ImageCreateForms(data=request.GET)
+        return render(request,
+                      "images/image/create.html",
+                      {"section":"images",
+                       "form": form})
+        
